c7513d1664560ffe3db61939767902df0331d6d3
Hridoy Munda <hridoymnd10@gmail.com>
2026-02-15 22:13:47 +0600

    added some common usefull event list

--- event_module/module_summary.js
+++ event_module/module_summary.js
@@ -2,4 +2,30 @@
 >> Onclick
 >> addEventListener 
 
+// Some event name 
+>> click
+>> doubleClick
+>> mouseenter
+>> mouseleave
+>> mousemove
+>> keyup
+>> keydown
+>> keypress
+>> submit
+>> change
+>> focus
+>> blur
+>> scroll
+>> resieze
+>> load
+>> play
+>> pause
+>> drag
+>> drop
+>> copy 
+>> cut 
+>> paste
+>> online
+>> offline
+
 */
